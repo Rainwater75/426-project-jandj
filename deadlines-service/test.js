64eb13a78fa224ce323bbed1587403d2710fd034
sh34v3 <jsok475@gmail.com>
2026-07-27 22:28:31 +0000

    update: added tests

--- deadlines-service/test.js
+++ deadlines-service/test.js
@@ -1,3 +1,12 @@
-const resp = await (await fetch("http://localhost:3002/gather_digests")).json();
+const gather_digests_resp = await (
+  await fetch("http://localhost:3002/gather_digests", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  })
+).json();
 
-console.log(JSON.stringify(resp));
+console.assert(gather_digests_resp.success === true);
+console.assert(gather_digests_resp.messageId);
+console.assert(gather_digests_resp.type === "topa_digest_notification");
+
+console.log("done testing");
