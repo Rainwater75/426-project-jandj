3859e5c58bc2b82d1c9789ffe85cc2bdd0721990
sh34v3 <jsok475@gmail.com>
2026-08-03 20:27:35 -0400

    add: load test and summary for liasion.check

--- load-tests/sprint-3-load.js
+++ load-tests/sprint-3-load.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, sleep } from "k6";
 
 export const options = {
   scenarios: {
@@ -15,13 +15,21 @@ export const options = {
       duration: "30s",
       exec: "getAssociation",
     },
+    liason: {
+      executor: "constant-vus",
+      vus: 20,
+      duration: '30s',
+      exec: "liasion",
+    }
   },
   thresholds: {
     'http_req_duration{scenario:getUser}': ['p(95)<10000'],
     'http_req_duration{scenario:getAssociation}': ['p(95)<10000'],
+    'http_req_duration{scenario:liason}': ['p(95)<2000'],
 
     'http_req_failed{scenario:getUser}': ['rate<0.01'],
     'http_req_failed{scenario:getAssociation}': ['rate<0.01'],
+    'http_req_failed{scenario:liason}': ['rate<0.01'],
   },
 };
 
@@ -33,4 +41,23 @@ export function getUser() {
 export function getAssociation() {
   http.get("http://localhost:3999/get_TA?ta_id=4");
   sleep(0.05);
+}
+
+const MOCK_PROFILES = [
+    { zip: '02108', neighborhood: 'Boston' },
+    { zip: '02138', neighborhood: 'Cambridge' },
+    { zip: '01002', neighborhood: 'Amherst' },
+    { zip: '99999', neighborhood: 'test' },
+];
+
+
+export function liasion(){
+  const randomProfile = MOCK_PROFILES[Math.floor(Math.random() * MOCK_PROFILES.length)];
+  // const url = 'http://localhost:8080/liaison/match?zipCode=02108&neighborhood=Boston';
+  const url = `http://localhost:8080/liaison/match?zipCode=${randomProfile.zip}&neighborhood=${randomProfile.neighborhood}`;
+
+  const res = http.get(url, {headers: { "Content-Type": "application/json" }});
+
+  check(res, { "status is 2xx": (r) => r.status >= 200 && r.status < 300 });
+  sleep(0.1);
 }
