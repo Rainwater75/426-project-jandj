2de796cf06ee07c06e3f3ae86d2c1881bbece4d4
sh34v3 <jsok475@gmail.com>
2026-08-09 11:53:44 -0400

    add: health checks, wired kafka startup script

---
+++ kafka-init/src/kafka-init.js
@@ -0,0 +1,11 @@
+// kafka-init.js
+import { Kafka } from "kafkajs";
+
+const KAFKA_BROKER = process.env.KAFKA_BROKER || "kafka:19092";
+
+const kafka = new Kafka({ brokers: [KAFKA_BROKER] });
+const admin = kafka.admin();
+await admin.connect();
+await admin.createTopics({ topics: [{ topic: "emails", numPartitions: 1 }] });
+await admin.disconnect();
+console.log("[LOG] Topic 'emails' created successfully.");
